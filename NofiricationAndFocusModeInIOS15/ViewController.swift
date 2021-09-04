3a77fd87b98e3f36e490aef57a5ef50596a9ff2d
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-09-04 20:23:20 +0300

    init

--- NofiricationAndFocusModeInIOS15/ViewController.swift
+++ NofiricationAndFocusModeInIOS15/ViewController.swift
@@ -11,9 +11,32 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        UNUserNotificationCenter.current().requestAuthorization(options: .alert) { _, _ in
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                self.scheduleNotification()
+            }
+        }
     }
 
-
+    func scheduleNotification() {
+        
+        let content = UNMutableNotificationContent()
+        content.title = "From SwiftUI"
+        content.body = "New Notification in iOS 15"
+        content.interruptionLevel = UNNotificationInterruptionLevel.active
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 3, repeats: false)
+        
+        let request = UNNotificationRequest(identifier: "iOS15", content: content, trigger: trigger)
+        
+        UNUserNotificationCenter.current().add(request) { error in
+            if let error = error {
+                print(error)
+            } else {
+                print("Success")
+            }
+        }
+    }
 }
 
